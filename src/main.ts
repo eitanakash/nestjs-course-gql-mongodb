cf95696bd52409305ed4241bca415719727cc96f
eitan.n <eitan.n@trgint.com>
2020-10-01 16:37:54 +0300

    adit readme, few comments

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe()); // to activate the validation class
   await app.listen(3000);
 }
 bootstrap();
